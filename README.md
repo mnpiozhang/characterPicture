0b578ece49303f77ef16657803830b5672dd6b12
mnpiozhang <cloudhzl@163.com>
2018-06-11 17:26:28 +0800

    增加生成PNG图片文件的脚本

--- README.md
+++ README.md
@@ -2,20 +2,25 @@
 彩色字符画转换工具
 
 	usage: trans.py [-h] [-i INPUTFILE] [-o OUTPUTFILE]
+	usage: transPNG.py [-h] [-i INPUTFILE] [-o OUTPUTFILE]
 	
-	trans pic to charpic
+	trans pic to charpic 
+	
 	
 	optional arguments:
 	  -h, --help     show this help message and exit
 	  -i INPUTFILE   input pic file
-	  -o OUTPUTFILE  output pic file,default output.html
+	  -o OUTPUTFILE  output pic file,default output.html or output.png
 
 说明
 ===========
 * 将图片转换为彩色字符画
 * -i 参数指定输入图片文件
-* -o 参数指定字符画输出文件，默认为当前目录下的output.html
-* 为调整长宽合适的比例，高度取像素点的步长为2
+* -o 参数指定字符画输出文件，默认为当前目录下的output.html或者output.png
+* 为调整长宽合适的比例，高度取像素点的步长默认为2
+* 使用分为两个版本，trans.py 脚本输出html文件   transPNG.py 脚本输出png图片   用法两者一样
+* 输出png的脚本需要安装phantomjs 而且默认运行在windows，如果需要运行在linux上需要修改下脚本内容
+
 
 example:
 
--- transPNG.py
+++ transPNG.py
@@ -6,11 +6,15 @@ import argparse
 import sys
 import os
 
+from selenium import webdriver
+
+
 parser = argparse.ArgumentParser(description="trans pic to charpic")
 parser.add_argument('-i',dest='inputfile',action='store',help='input pic file')
-parser.add_argument('-o',dest='outputfile',action='store',help='output pic file,default output.html')
+parser.add_argument('-o',dest='outputfile',action='store',help='output pic file,default output.html or output.png')
 parser.add_argument('-hs',dest='height_step',action='store',help='read pic height step')
 parser.add_argument('-ws',dest='width_step',action='store',help='read pic width  step')
+
 args = parser.parse_args()
 
 str1 = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'.")
@@ -19,6 +23,13 @@ str1 = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^
 def get_char(r, g, b):
     gray = int(0.2126 * r + 0.7152 * g + 0.0722 * b)
     return str1[int(gray / 256.0 * len(str1))]
+
+def create_PNG_file(htmlfile,outputPNGfile):
+    driver=webdriver.PhantomJS(executable_path="phantomjs.exe")
+    driver.get("file:///" + os.path.join(os.getcwd(),htmlfile))  
+    driver.save_screenshot(outputPNGfile)
+    driver.quit()
+
 
 if __name__ == '__main__':
     if args.inputfile and os.path.exists(args.inputfile):
@@ -39,7 +50,7 @@ if __name__ == '__main__':
     except:
         print "height step or width step format error"
         sys.exit()
-        
+
     w,h = im.size
     resulthtml = '<div style="font-size:0.48em;">'
     for j in range(0,h,height_step):
@@ -51,8 +62,10 @@ if __name__ == '__main__':
         resulthtml = resulthtml + '<br>'
     resulthtml = resulthtml + '</div>'
     if args.outputfile:
-        with open(args.outputfile,'w') as f:
+        with open("tmpoutput.html",'w') as f:
             f.write(resulthtml)
+        create_PNG_file("tmpoutput.html",args.outputfile)
     else:
-        with open("output.html",'w') as f:
+        with open("tmpoutput.html",'w') as f:
             f.write(resulthtml)
+        create_PNG_file('tmpoutput.html','output.png')
